2b36be79ba40b0a0d98f3ad2e4c11d448676b794
Yeliz Kececi <yelizkececi>
2020-11-24 23:02:00 +0300

    Initial Commit

---
+++ GestureRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizer
+//
+//  Created by Yeliz Keçeci on 24.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
